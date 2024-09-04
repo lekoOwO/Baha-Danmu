6b373d52c4679cb580213d91a92d51feca31c741
Leko <leko@leko.moe>
2024-09-05 02:33:06 +0800

    fix: dir location

--- src/utils/env.py
+++ src/utils/env.py
@@ -4,7 +4,9 @@ import pathlib
 VIDEO_BASE_DIR = pathlib.Path(os.getenv('VIDEO_BASE_DIR', '/data/videos')).absolute()
 assert(VIDEO_BASE_DIR.exists())
 
-STATIC_DIR = pathlib.Path("src/static").absolute()
+# Get this file's parent directory
+SRC_DIR = pathlib.Path(__file__).parent.parent.absolute()
+STATIC_DIR = (SRC_DIR / "static").absolute()
 assert(STATIC_DIR.exists())
 
 GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
